13de03a45bc1ad22c05de901e44459ca9b7ab4ab
Jaroslav Barton <jaroslav@bartonovi.eu>
2020-04-28 17:38:07 +0200

    Download latest exports using HTTP

--- pyasn-utils/pyasn_util_download.py
+++ pyasn-utils/pyasn_util_download.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # Copyright (c) 2009-2017 Hadi Asghari
 #
@@ -20,159 +20,120 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Script to download the latest routeview bgpdata, or for a certain period
-# Thanks to Vitaly Khamin (https://github.com/khamin) for the FTP code
+# Script to download the latest routeview bgpdata
 
 from __future__ import print_function, division
-from datetime import date, datetime
-from time import time
-from ftplib import FTP
+from html.parser import HTMLParser
+import re
 from argparse import ArgumentParser
-from subprocess import call
-from sys import argv, exit, stdout, version_info
+
 try:
     from pyasn import __version__
-except:
+except ImportError:
+    __version__ = "N/A"
     pass  # not fatal if we can't get version
-if version_info[0] < 3:
-    from urllib2 import urlopen
-else:
-    from urllib.request import urlopen
 
+import urllib.request
 
 # Parse command line options
-# Note: --latest might be changes to --46, instead of --4, in the future
 parser = ArgumentParser(description="Script to download MRT/RIB BGP archives (from RouteViews).")
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('--latestv4', '-4', '--latest', action='store_true',
-                   help='Grab lastest IPV4 data')
-group.add_argument('--latestv6', '-6', action='store_true', help='Grab lastest IPV6 data')
-group.add_argument('--latestv46', '-46', action='store_true', help='Grab lastest IPV4/V6 data')
-group.add_argument('--version', action='store_true')
-group.add_argument('--dates-from-file', '-f', action='store',
-                   help='Grab IPV4 archives for specifc dates (one date, YYYYMMDD, per line)')
+parser.add_argument('--latestv4', '-4', action='store_true', help='Grab lastest IPV4 data')
+parser.add_argument('--latestv6', '-6', action='store_true', help='Grab lastest IPV6 data')
+parser.add_argument('--version', action='store_true')
+
 args = parser.parse_args()
 
 
-def ftp_download(server, remote_dir, remote_file, local_file, print_progress=True):
-    """Downloads a file from an FTP server and stores it locally"""
-    ftp = FTP(server)
-    ftp.login()
-    ftp.cwd(remote_dir)
-    if print_progress:
-        print('Downloading ftp://%s/%s/%s' % (server, remote_dir, remote_file))
-    filesize = ftp.size(remote_file)
-    # perhaps warn before overwriting file?
-    with open(local_file, 'wb') as fp:
-        def recv(s):
-            fp.write(s)
-            recv.chunk += 1
-            recv.bytes += len(s)
-            if recv.chunk % 100 == 0 and print_progress:
-                print('\r %.f%%, %.fKB/s' % (recv.bytes*100 / filesize,
-                      recv.bytes / (1000*(time()-recv.start))), end='')
-                stdout.flush()
-        recv.chunk, recv.bytes, recv.start = 0, 0, time()
-        ftp.retrbinary('RETR %s' % remote_file, recv)
-    ftp.close()
-    if print_progress:
-        print('\nDownload complete.')
+# create a subclass and override the handler methods
+class BgpSiteHtmlParser(HTMLParser):
+    def error(self, message):
+        super().error(message)
+
+    def __init__(self, year_month: bool = True):
+        super().__init__()
+        self.latest_timestamp_pt_1: str = "-1"
+        self.latest_timestamp_pt_2: str = "-1"
+        if year_month:
+            self.regex = re.compile('(\\d{4})\\.(\\d{2})')
+        else:
+            self.regex = re.compile('rib\\.(\\d{8})\\.(\\d{4}).bz2')
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for (attr, value) in attrs:
+                if attr == 'href':
+                    match = self.regex.match(value)
+                    if match:
+                        if int(match.group(1)) > int(self.latest_timestamp_pt_1):
+                            self.latest_timestamp_pt_1 = match.group(1)
+                            self.latest_timestamp_pt_2 = match.group(2)
+                        if int(match.group(1)) == int(self.latest_timestamp_pt_1) and \
+                                int(match.group(2)) > int(self.latest_timestamp_pt_2):
+                            self.latest_timestamp_pt_2 = match.group(2)
+
+    def get_latest_year_month(self):
+        return "{}.{}/RIBS".format(self.latest_timestamp_pt_1, self.latest_timestamp_pt_2)
+
+    def get_latest_file_name(self):
+        return "rib.{}.{}.bz2".format(self.latest_timestamp_pt_1, self.latest_timestamp_pt_2)
 
 
-def find_latest_in_ftp(server, archive_root, sub_dir, print_progress=True):
-    """Returns (server, filepath, filename) for the most recent file in an FTP archive"""
-    if print_progress:
-        print('Connecting to ftp://' + server)
-    ftp = FTP(server)
-    ftp.login()
-    months = sorted(ftp.nlst(archive_root), reverse=True)  # e.g. 'route-views6/bgpdata/2016.12'
-    filepath = '/%s/%s' % (months[0], sub_dir)
-    if print_progress:
-        print("Finding most recent archive in %s ..." % filepath)
-    ftp.cwd(filepath)
-    fls = ftp.nlst()
-    if not fls:
-        filepath = '/%s/%s' % (months[1], sub_dir)
-        if print_progress:
-            print("Finding most recent archive in %s ..." % filepath)
-        ftp.cwd(filepath)
-        fls = ftp.nlst()
-        if not fls:
-            raise LookupError("Cannot find file to download. Please report a bug on github?")
-    filename = max(fls)
-    ftp.close()
-    return (server, filepath, filename)
+def get_html(requested_url: str):
+    return str(urllib.request.urlopen(requested_url).read(), 'ascii')
+
+
+def get_latest_year_month_url(requested_url: str):
+    bgp_data_parser = BgpSiteHtmlParser()
+    bgp_data_parser.feed(get_html(requested_url))
+    return "{}/{}".format(requested_url, bgp_data_parser.get_latest_year_month())
+
+
+def get_latest_file_url(requested_url: str):
+    bgp_data_parser = BgpSiteHtmlParser(False)
+    bgp_data_parser.feed(get_html(requested_url))
+    return "{}/{}".format(requested_url, bgp_data_parser.get_latest_file_name())
+
+
+def find_latest_in_web(server: str, archive_root: str):
+    base_url = "http://{}/{}".format(server, archive_root)
+    latest_year_month_url = get_latest_year_month_url(base_url)
+    latest_file_url = get_latest_file_url(latest_year_month_url)
+    return latest_file_url
+
+
+def download_report_hook(blocknum, bs, size):
+    if blocknum % 10 == 0 or blocknum * bs >= size:
+        print('\r %.2f%%' % min(((blocknum * bs) / size) * 100, 100), end='')
+
+
+def web_download(requested_url: str, ipv: str):
+    print("Downloading {} ...".format(requested_url))
+    urllib.request.urlretrieve(requested_url, "v{}-{}".format(ipv, requested_url.split("/")[-1]), download_report_hook)
+    print("\n ... done")
 
 
 def find_latest_routeviews(archive_ipv):
     # RouteViews archives are as follows:
-    # ftp://archive.routeviews.org/datapath/YYYYMM/ribs/XXXX
+    # http://archive.routeviews.org/datapath/YYYYMM/ribs/XXXX
     archive_ipv = str(archive_ipv)
-    assert archive_ipv in ('4', '6', '46', '64')
-    return find_latest_in_ftp(server='archive.routeviews.org',
+    assert archive_ipv in ('4', '6')
+    return find_latest_in_web(server='archive.routeviews.org',
                               archive_root='bgpdata' if archive_ipv == '4' else
-                                           'route-views6/bgpdata' if archive_ipv == '6' else
-                                           'route-views4/bgpdata',  # 4+6
-                              sub_dir='RIBS')
+                              'route-views6/bgpdata'
+                              )
+
+
+def download_latest_for_version(ipv: str):
+    url = find_latest_routeviews(ipv)
+    web_download(url, ipv)
 
 
 if args.version:
     print("MRT/RIB downloader version %s." % __version__)
 
-
-if args.latestv4 or args.latestv6 or args.latestv46:
+if args.latestv4:
     # Download latest RouteViews MRT/RIB archive
-    srvr, rp, fn = find_latest_routeviews(4 if args.latestv4 else 6 if args.latestv6 else '46')
-    ftp_download(srvr, rp, fn, fn)
+    download_latest_for_version('4')
 
-
-if args.dates_from_file:
-    # read dates from a local file and use wget to download range
-    dates_to_get = []
-    f = open(args.dates_from_file)
-    if not f:
-        print("can't open %s" % args.dates_from_file)
-        exit()
-    for s in f:
-        if not s.strip() or s[0] == '#':
-            continue
-        dt = date(int(s[:4]), int(s[4:6]), int(s[6:8]))  # Dates are strangely YYYYMMDD :)
-        dates_to_get.append(dt)
-
-    for dt in dates_to_get:
-        # FIXME: currently v4 only. should understand v4/v6 options, and possibly use FTP method
-        url_dir = 'http://archive.routeviews.org/bgpdata/%d.%02d/RIBS/' % (dt.year, dt.month)
-        print('Searching %s for %d-%02d-%02d...' % (url_dir, dt.year, dt.month, dt.day), end=' ')
-        stdout.flush()
-
-        html = str(urlopen(url_dir).read())
-        str_find = 'rib.%d%02d%02d' % (dt.year, dt.month, dt.day)
-
-        ix = html.find(str_find + '.06')  # get the file saved at 6 AM for consistency
-        if ix == -1:
-            ix = html.find(str_find + '.05')  # if not, try 5 AM
-            if ix == -1:
-                ix = html.find(str_find + '.00')  # last resort, try the one saved at midnight
-                if ix == -1:
-                    print('=> ERROR - NOT FOUND.')
-                    continue
-
-        fname = html[ix:ix+21]
-        s = html[ix+80:ix+150]
-        ix = s.find('"right"')
-        assert ix != -1
-        s = s[ix+8:]
-        ix = s.find("</td>")
-        assert ix != -1
-        size = s[:ix]
-
-        url_full = url_dir + fname
-        print('downloading...', end=' ')
-        stdout.flush()
-        # FIXME: Why using urllib AND wget? Can urllib do listing AND downloading? (OR ftp...)
-        ret = call(['wget', '-q', url_full])  # wget in quiet mode
-        print()
-        ret = "" if ret == 0 else "[FAIL:%d]" % ret
-
-        print('%s\t%s\t%s\t%s' % (dt, size, url_full, ret))
-        stdout.flush()
+if args.latestv6:
+    download_latest_for_version('6')
